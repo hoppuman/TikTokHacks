4931d3bbe07cfdf1051594fe467c98e67f503ed7
James Hoffman <james.a.hoffman@stonybrook.edu>
2020-05-28 22:17:56 -0400

    updated with less imports

--- Shrek/shrek.py
+++ Shrek/shrek.py
@@ -1,19 +1,12 @@
 import time
-import datetime
-
-from bs4 import BeautifulSoup
-import urllib
-
-from pyautogui import press, typewrite, hotkey
 import pyautogui
 
 def SendScript():
-	time.sleep(1)
+	time.sleep(2)
 	with open('script.txt') as f:
 		lines = f.readlines()
-
 	for line in lines:
-		typewrite(line.strip())
+		pyautogui.typewrite(line.strip())
 		pyautogui.press('enter')
 
 SendScript()
