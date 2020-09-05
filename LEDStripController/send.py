c7e15882a2dfc35415d05ebebde8231c8fc1e574
James Hoffman <james.a.hoffman@stonybrook.edu>
2020-09-05 12:38:13 -0400

    upating server code from stream

--- LEDStripController/send.py
+++ LEDStripController/send.py
@@ -20,6 +20,29 @@ def sendNewColorToPi(red, green, blue):
 	dictToSend = {'red':red,'green':green, 'blue':blue}
 	res = requests.post(url, data=dictToSend)
 
+def sendColorWipe(red, green, blue):
+	url = 'http://192.168.1.240:5000/colorWipeVariable'
+	dictToSend = {'red':red,'green':green, 'blue':blue}
+	res = requests.post(url, data=dictToSend)
+
+def sendColorWipeContinuous(red, green, blue):
+	url = 'http://192.168.1.240:5000/colorWipeContinuous'
+	dictToSend = {'red':red,'green':green, 'blue':blue}
+	res = requests.post(url, data=dictToSend)
+
+def sendColorGradient():
+	red_beg = 0
+	green_beg = 0
+	blue_beg = 255
+
+	red_end = 100
+	green_end = 0
+	blue_end = 255
+
+	url = 'http://192.168.1.240:5000/colorWipeChange'
+	dictToSend = {'red_beg':red_beg,'green_beg':green_beg, 'blue_beg':blue_beg, 'red_end':red_end,'green_end':green_end, 'blue_end':blue_end}
+	res = requests.post(url, data=dictToSend)
+
 def testCall():
 	url = 'http://192.168.1.240:5000/test'
 	dictToSend = {'red':'red','blue':'blue','green':'green'}
@@ -85,6 +108,25 @@ def readToken():
 		# return my token
 		return lines[0]
 
+def GenerateRGBValuesFromColor(color):
+	print("color I'm looking for is: " , color)
+	if(color == "yellow"):
+		return 30,80,0
+	elif (color == "purple"):
+		 return 10,0,30
+	elif (color == "pink"):
+		 return 20,0,20
+	elif (color == "white"):
+		 return 10,20,50
+	elif (color == "red"):
+		 return 255,0,0
+	elif (color == "blue"):
+		 return 0,0,255
+	elif (color == "green"):
+		 return 0,255,0
+	else:
+		return -1,-1,-1
+
 def readTwitchChat():
 	
 	sock = socket.socket()
@@ -102,19 +144,33 @@ def readTwitchChat():
 
 	# !color 255,0,0
 	while(True):
+		resp = sock.recv(2048).decode('utf-8')
+
+		if resp.startswith('PING'):
+			sock.send("PONG\n".encode('utf-8'))
+
+		chatParts = resp.split(":")
+		if(len(chatParts) < 3):
+			continue
+		message = chatParts[2]
+		print(message)
+		rgb = ""
 		try:
-			resp = sock.recv(2048).decode('utf-8')
-			chatParts = resp.split(":")
-			message = chatParts[2]
-			rgb = ""
 			if(message.startswith('!color')):
 				color = message.split(" ")[1]
-				colorSplit = color.split(",")
-				red = colorSplit[0]
-				green = colorSplit[1]
-				blue = colorSplit[2]
-				sendNewColorToPi(red,green,blue)
-				print("red: " , red, "green: " , green, "blue: " , blue)
+				print(color)
+
+				red,green,blue = GenerateRGBValuesFromColor(color.strip())
+				print(red," ",green," ",blue)
+				if(red != -1):
+					sendNewColorToPi(red,green,blue)
+				else:
+					colorSplit = color.split(",")
+					red = colorSplit[0]
+					green = colorSplit[1]
+					blue = colorSplit[2]
+					sendNewColorToPi(red,green,blue)
+					print("red: " , red, "green: " , green, "blue: " , blue)
 		except:
 			print("no second message")
 
@@ -123,7 +179,8 @@ def readTwitchChat():
 
 	sock.close()
 
-
-testAllColors()
-
+#sendColorWipe(255,0,0)
+#dColorWipeContinuous(0,0,255)
 #testAllColors()
+#readTwitchChat()
+sendColorGradient()
