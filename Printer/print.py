61e84dbb4a577091d660bc75b9f3a22614813a54
James Hoffman <jhoffman1204@gmail.com>
2021-05-17 15:38:32 -0700

    added more weather data, removed printer stuf

--- Printer/print.py
+++
@@ -1,6 +0,0 @@
-import os
-import time
-
-time.sleep(5)
-
-os.startfile("C:/Users/James/Documents/coding/python projects/Printer/Book1.xlsx", "print")
--- Printer/server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-	return 'hello'
-	
-if __name__ == "__main__":
-	app.run(port=8080)
